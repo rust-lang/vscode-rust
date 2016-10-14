16812327b8e8f5ab01c916c66ad24c2c008482c8
Jonathan Turner <jturner@mozilla.com>
2016-10-14 11:59:59 -0700

    Remove stderr in console. Add status messages

--- ls_client/src/extension.ts
+++ ls_client/src/extension.ts
@@ -2,14 +2,22 @@
 
 import * as path from 'path';
 
-import { workspace, Disposable, ExtensionContext, languages } from 'vscode';
+import * as child_process from 'child_process';
+
+import { workspace, Disposable, ExtensionContext, languages, window } from 'vscode';
 import { LanguageClient, LanguageClientOptions, SettingMonitor, ServerOptions, TransportKind } from 'vscode-languageclient';
 
 export function activate(context: ExtensionContext) {
- 	let serverOptions: ServerOptions = {
-		run: {command: "rls"},
-		debug: {command: "rls"}
-	};
+    const serverOptions = () => new Promise<child_process.ChildProcess>((resolve, reject) => {
+        function spawnServer(...args: string[]): child_process.ChildProcess {
+			let childProcess = child_process.spawn("rls");
+			childProcess.stderr.on('data', data => {});
+			return childProcess; // Uses stdin/stdout for communication
+		}
+
+		resolve(spawnServer())
+	});
+
 	// Options to control the language client
 	let clientOptions: LanguageClientOptions = {
 		// Register the server for Rust files
@@ -19,11 +27,28 @@ export function activate(context: ExtensionContext) {
 			configurationSection: 'languageServerExample',
 			// Notify the server about changes to files contained in the workspace
 			//fileEvents: workspace.createFileSystemWatcher('**/*.*')
+		},
+		errorHandler: {
+			closed: function(): any {
+
+			},
+
+			error(error: Error, message: any, count: number): any {
+
+			}
 		}
 	}
 	
 	// Create the language client and start the client.
-	let disposable = new LanguageClient('Rust Language Service', serverOptions, clientOptions).start();
+	let lc = new LanguageClient('Rust Language Service', serverOptions, clientOptions);
+
+	lc.onNotification({method: "rustDocument/diagnosticsBegin"}, function(f) {
+		window.setStatusBarMessage("RLS analysis: started");
+	})
+	lc.onNotification({method: "rustDocument/diagnosticsEnd"}, function(f) {
+		window.setStatusBarMessage("RLS analysis: done");
+	})
+	let disposable = lc.start();
 
 	// Push the disposable to the context's subscriptions so that the 
 	// client can be deactivated on extension deactivation

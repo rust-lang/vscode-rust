48d2dcc5d998dcb0757339ddd9459384e26a8615
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2017-07-18 16:36:07 +0900

    Add promise wrappers for child_process modules.

---
+++ src/utils/child_process.ts
@@ -0,0 +1,40 @@
+// Copyright 2017 The RLS Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+'use strict';
+
+/**
+ *  Ideally, this module should be replaced with `utils.promisefy()` introduced Node.js 8.
+ *  But we might not replace these properly if it is not well typed.
+ */
+
+import * as child_process from 'child_process';
+
+export interface ExecChildProcessResult<TOut = string> {
+    readonly stdout: TOut;
+    readonly stderr: TOut;
+}
+
+export function execChildProcess(command: string): Promise<ExecChildProcessResult> {
+    const r: Promise<ExecChildProcessResult> = new Promise((resolve, reject) => {
+        child_process.exec(command, (error, stdout, stderr) => {
+            if (!!error) {
+                reject(error);
+                return;
+            }
+
+            resolve({
+                stdout,
+                stderr,
+            });
+        });
+    });
+    return r;
+}

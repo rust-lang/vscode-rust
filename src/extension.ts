1159e07ee7b109d0bbc103c73c55d78e105f3bab
Kim Christensen <kimworking@gmail.com>
2017-12-14 22:42:07 +0100

    Do not start RLS if no folder was opened

--- src/extension.ts
+++ src/extension.ts
@@ -143,6 +143,10 @@ export function activate(context: ExtensionContext) {
 
 function startLanguageClient(context: ExtensionContext)
 {
+    if (workspace.rootPath === undefined) {
+        window.showWarningMessage('Cannot start RLS because no folder was opened');
+        return;
+    }
     warnOnMissingCargoToml();
 
     window.setStatusBarMessage('RLS: starting up');

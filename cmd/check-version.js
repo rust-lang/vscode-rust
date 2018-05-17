00b40b02f8ad2789a17cefb89d0476f27d74872c
Tomáš Hübelbauer <tomas@hubelbauer.net>
2018-05-17 18:49:54 +0200

    Implement checking changelog for corresponding version entry

---
+++ cmd/check-version.js
@@ -0,0 +1,5 @@
+const { version } = require('../package');
+const changelog = String(require('fs').readFileSync('CHANGELOG.md')).split('\n');
+if (!changelog.find(line => line.startsWith('### ' + version))) {
+  throw new Error(`The package.json version ${version} does not seem to have a matching heading in CHANGELOG.json`);
+}

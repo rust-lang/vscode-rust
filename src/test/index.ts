e30afa914b9dba610b2ba5369f064d63a695c8dc
Igor Matuszewski <Xanewok@gmail.com>
2019-03-27 23:11:32 +0100

    Fix TSLint lints in the test scaffolding

--- src/test/index.ts
+++ src/test/index.ts
@@ -10,14 +10,15 @@
 // to report the results back to the caller. When the tests are finished, return
 // a possible error to the callback or null if none.
 
+// tslint:disable-next-line: no-submodule-imports
 import * as testRunner from 'vscode/lib/testrunner';
 
 // You can directly control Mocha options by configuring the test runner below
 // See https://github.com/mochajs/mocha/wiki/Using-mocha-programmatically#set-options
 // for more info
 testRunner.configure({
-    ui: 'tdd', 		// the TDD UI is being used in extension.test.ts (suite, test, etc.)
-    useColors: true // colored output from test results
+  ui: 'tdd', // the TDD UI is being used in extension.test.ts (suite, test, etc.)
+  useColors: true, // colored output from test results
 });
 
 module.exports = testRunner;

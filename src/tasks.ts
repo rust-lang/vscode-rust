45815810443d7395b828bc51409e76008cbaec38
Jannick Johnsen <jannickboese@msn.com>
2020-04-13 08:18:00 +0200

    set cwd to be the workspace folder, fixes cargo build command for "enableMultiProjectSetup" feature.

--- src/tasks.ts
+++ src/tasks.ts
@@ -78,7 +78,7 @@ function detectCargoTasks(target: WorkspaceFolder): Task[] {
     .map(({ subcommand, group }) => ({
       definition: { subcommand, type: TASK_TYPE },
       label: `cargo ${subcommand}`,
-      execution: createShellExecution({ command: 'cargo', args: [subcommand] }),
+      execution: createShellExecution({ command: 'cargo', args: [subcommand], cwd: target.uri.fsPath }),
       group,
       problemMatchers: ['$rustc'],
     }))

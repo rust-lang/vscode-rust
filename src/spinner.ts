0bd2d92382a3737553c9ac222f0eec61871ee7d7
Igor Matuszewski <xanewok@gmail.com>
2020-04-24 13:54:50 +0200

    Use octicon spinner in status bar item

--- src/spinner.ts
+++ src/spinner.ts
@@ -1,24 +1,9 @@
 import { window } from 'vscode';
 
 export function startSpinner(prefix: string, postfix: string) {
-  if (spinnerTimer != null) {
-    clearInterval(spinnerTimer);
-  }
-  let state = 0;
-  spinnerTimer = setInterval(() => {
-    window.setStatusBarMessage(`${prefix} ${spinner[state]} ${postfix}`);
-    state = (state + 1) % spinner.length;
-  }, 100);
+  window.setStatusBarMessage(`${prefix} $(settings-gear~spin) ${postfix}`);
 }
 
 export function stopSpinner(message: string) {
-  if (spinnerTimer) {
-    clearInterval(spinnerTimer);
-  }
-  spinnerTimer = null;
-
   window.setStatusBarMessage(message);
 }
-
-let spinnerTimer: NodeJS.Timer | null = null;
-const spinner = ['🌕', '🌖', '🌗', '🌘', '🌑', '🌒', '🌓', '🌔'];
